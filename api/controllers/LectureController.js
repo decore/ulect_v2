76216c8e14525d7c6cf3e813061359efb948d65e
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2014-12-02 14:13:12 +0800

    fix lectures migrate data

--- api/controllers/LectureController.js
+++ api/controllers/LectureController.js
@@ -4,7 +4,7 @@
  * @description :: Server-side logic for managing lectures
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
-var dataMigrate =  require( 'data_ulect_migrate');
+var dataMigrate =  require( './data_ulect_migrate');
 module.exports = {
     migrate: function (req, res) {
         Lecture.create(dataMigrate,function(err,entites){
