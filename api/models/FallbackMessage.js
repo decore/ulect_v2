e8a0b791eab515ca7cdcad18031a47f31cb17cc6
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2014-12-12 09:49:55 +0700

    default code generation "sails generation api FallbackMessageController"

---
+++ api/models/FallbackMessage.js
@@ -0,0 +1,57 @@
+/**
+* FallbackMessage.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+          ToCountry: "string", // "US",
+ErrorUrl: "string", // "http://73eafd5e.ngrok.com/api/v1/messages/client",
+ToState: "string", // "CA",
+SmsMessageSid: "string", // "SMbf753bb01c5c68f160a03e2ce773d4c0",
+ErrorCode: "string", // "11200",
+NumMedia: "string", // "0",
+ToCity: "string", // "MOUNTAIN VIEW",
+FromZip: "string", // "",
+SmsSid: "string", // "SMbf753bb01c5c68f160a03e2ce773d4c0",
+FromState: "string", // "London",
+SmsStatus: "string", // "received",
+FromCity: "string", // "",
+Body: "string", // "Sent from your Twilio trial account - test callback 4",
+FromCountry: "string", // "GB",
+To: "string", // "+16505675330",
+ToZip: "string", // "94024",
+MessageSid: "string", // "SMbf753bb01c5c68f160a03e2ce773d4c0",
+AccountSid: "string", // "AC220dd9ec0df20b77d7cdd306ee34f43a",
+From: "string", // "+442033223499",
+ApiVersion: "string", //
+  }
+};
+/** Fallback message example
+ToCountry: "US",
+ErrorUrl: "http://73eafd5e.ngrok.com/api/v1/messages/client",
+ToState: "CA",
+SmsMessageSid: "SMbf753bb01c5c68f160a03e2ce773d4c0",
+ErrorCode: "11200",
+NumMedia: "0",
+ToCity: "MOUNTAIN VIEW",
+FromZip: "",
+SmsSid: "SMbf753bb01c5c68f160a03e2ce773d4c0",
+FromState: "London",
+SmsStatus: "received",
+FromCity: "",
+Body: "Sent from your Twilio trial account - test callback 4",
+FromCountry: "GB",
+To: "+16505675330",
+ToZip: "94024",
+MessageSid: "SMbf753bb01c5c68f160a03e2ce773d4c0",
+AccountSid: "AC220dd9ec0df20b77d7cdd306ee34f43a",
+From: "+442033223499",
+ApiVersion: "2010-04-01",
+createdAt: "2014-12-12T02:43:08.996Z",
+updatedAt: "2014-12-12T02:43:08.996Z",
+ 
+*/
